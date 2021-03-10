700459ff0fa37d594329ffe3d87f49c7b87cdb6c
MuhammadAkram <akram@channelwebconsulting.com>
2021-03-10 16:16:19 +0530

    add new file

---
+++ AirportHUD/Classes/ArrayExtension.swift
@@ -0,0 +1,12 @@
+//
+//  ArrayExtension.swift
+//  AirportHUD
+//
+//  Created by shardendu singh on 10/03/21.
+//
+
+import Foundation
+
+func addTwoNumbers(number1: Int, number2: Int) -> Int {
+    return number1 + number2
+}
